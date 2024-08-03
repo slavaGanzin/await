cad42c680346d12b89aca2dfe981610897083c5e
Slava <slava.ganzin@gmail.com>
2024-08-03 17:24:01 +0200

    add autocompletions

---
+++ autocompletions/await.bash
@@ -0,0 +1,25 @@
+_await() {
+    local cur prev opts
+    COMPREPLY=()
+    cur="${COMP_WORDS[COMP_CWORD]}"
+    prev="${COMP_WORDS[COMP_CWORD-1]}"
+
+    opts="--help --stdout --silent --fail --status --any --change --exec --interval --forever --service --version"
+
+    case "${prev}" in
+        --status|--exec|--interval)
+            COMPREPLY=($(compgen -f -- "${cur}"))
+            return 0
+            ;;
+    esac
+
+    if [[ ${cur} == -* ]]; then
+        COMPREPLY=($(compgen -W "${opts}" -- "${cur}"))
+        return 0
+    fi
+
+    COMPREPLY=($(compgen -c -- "${cur}"))
+    return 0
+}
+
+complete -F _await await
---
+++ autocompletions/await.zsh
@@ -0,0 +1,23 @@
+# Ensure compinit is loaded
+autoload -Uz compinit
+compinit
+
+# Define the completion function for 'await'
+_await() {
+  _arguments -s -S \
+    '--help[Print this help]' \
+    '--version[Display version]' \
+    '--stdout[Print stdout of commands]' \
+    '--silent[Do not print spinners and commands]' \
+    '--fail[Wait for commands to fail]' \
+    '--status[Expected status (default: 0)]::status' \
+    '--any[Terminate if any command returns expected status]' \
+    '--change[Wait for stdout to change and ignore status codes]' \
+    '--exec[Run some shell command on success]::command:_command_names' \
+    '--interval[Milliseconds between rounds of commands (default: 200)]::interval' \
+    '--forever[Do not exit ever]' \
+    '--service[Create systemd user service with same parameters and activate it]'
+}
+
+# Register the completion function
+compdef _await await
